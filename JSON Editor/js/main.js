4786b22d271a9deaa6e801758fac6530ee6e04d1
phoochka <phoochka@gmail.com>
2015-05-09 14:33:55 -0400

    shit not working

--- JSON Editor/js/main.js
+++ JSON Editor/js/main.js
@@ -2,23 +2,15 @@ JSONEditor.plugins.select2.width = "300px";
 JSONEditor.defaults.options.theme = 'bootstrap3';
 
 var editor = new JSONEditor(document.getElementById('editor_holder'), {
+    ajax: true,
+
     schema: {
-        type: "object",
-        title: "Tweet Text",
-        properties: {
-            Id: {
-                type: "integer",
-            },
-            Text: {
-                type: "string",
-            },
-            Class: {
-                type: "string",
-                enum: [
-                    "Positive",
-                    "Negative"
-                ]
-            }
+        type: "array",
+        title: "Tweet Texts",
+        items: {
+            title: "Tweet",
+            headerTemplate:"{{i}} - {{self.name}}",
+            $ref: "influential.json"
         }
     }
 });
