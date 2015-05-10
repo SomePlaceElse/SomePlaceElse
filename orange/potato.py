218dfe5a68901f68958ac76046df0fcd6ce1f9ed
Samarth Shah <samarthmshah@gmail.com>
2015-05-10 11:33:41 -0400

    to do list updated for the day

--- orange/potato.py
+++ orange/potato.py
@@ -1,17 +1,11 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import Orange
-
 
 # Load data from the text file: data.basket
 data = Orange.data.Table("data.basket")
 
-
 # Identify association rules with supports at least 0.3
-rules = Orange.associate.AssociationRulesSparseInducer(data, support=0.2)
+# Since our basket is sparse.
+rules = Orange.associate.AssociationRulesSparseInducer(data, support=0.3)
 
 
 # print out rules
@@ -19,6 +13,8 @@ print "%4s %4s  %s" % ("Supp", "Conf", "Rule")
 for r in rules[:]:
     print "%4.1f %4.1f  %s" % (r.support, r.confidence, r)
 
+print 'Rules ', rules
+print 'Rules[0] ', rules[0]
 rule = rules[0]
 for index, d in enumerate(data):
     # print '\nUser {0}: {1}'.format(index, raw_data[idx])
@@ -26,3 +22,7 @@ for index, d in enumerate(data):
         if r.applies_left(d) and not r.applies_right(d):
             print "%4.1f %4.1f  %s" % (r.support, r.confidence, r)
 
+# Now set a minimum threshold on confidence. i.e. 0.5 50% of the time the rule is correct
+# We can also optimize it y considering only those set of rules where user is concerned
+# set a minRules and maxRules so that if the # of rules returned = maxRules, it means the support is too low.
+# vice versa for minRules
