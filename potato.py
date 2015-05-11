dd6fae6522a760bbf133529d539c691d2477aa71
phoochka <phoochka@gmail.com>
2015-05-10 22:11:42 -0400

    moved potato folder to root dir to GET THINGS DONE

--- potato.py
+++ potato.py
@@ -18,7 +18,7 @@ class Potato:
         )
         data = Data([trace0, trace1])
 
-        self.unique_url = py.plot(data, filename = 'basic-line')
-        self.embed_url  = self.unique_url + '.embed?width=750&height=550'
-        self.html_url   = self.unique_url + '.html' 
+        self.unique_url = py.plot(data, filename = 'basic-line', auto_open=False)
+        self.embed_url = self.unique_url + '.embed?width=750&height=550'
+        self.html_url  = self.unique_url + '.html' 
         #return self.unique_url
--- web.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def potato():
-    return 'Go eat potato!'
-
-if __name__ == "__main__":
-    app.run()
